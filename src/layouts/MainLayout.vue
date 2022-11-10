3e24b4612dc5141bc0b63a597887864954f35f09
Alexandr Iv <ivanin.alexandr@outlook.com>
2022-11-10 17:36:54 +0300

    start project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="lHh Lpr lFf"> </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
